bdeb651b65fcefc855ea3cfdd247859f13dfbec9
Chandan Kuilya <67276477+ChandanKuilya@users.noreply.github.com>
2025-08-11 14:03:20 +0530

    initial commit

---
+++ src/test/java/com/myproj/ecommercespringck01/EcommerceSpringCk01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproj.ecommercespringck01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringCk01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
